02881fe4ec75bd676b05f2e54b32f90b248175b2
luca-masera <lucamasera1995@gmail.com>
2023-11-29 17:59:36 +0100

    bonus 2

--- partials/header.php
+++ partials/header.php
@@ -1,12 +1,14 @@
 <?php include __DIR__ . '/../model/dati.php';
 //var_dump($hotels)
-if (isset($_GET['parking'])) {
+
+if (isset($_GET['parking']) . ($_GET['vote'])) {
     $parking = $_GET['parking'];
+    $vote = $_GET['vote'];
     $temp_array = [];
 
     foreach ($hotels as $hotel) {
 
-        if ($hotel['parking'] === (bool) $parking) {
+        if ($hotel['parking'] === (bool) $parking && $hotel['vote'] >= 3) {
             $temp_array[] = $hotel;
         }
 
@@ -19,10 +21,11 @@ if (isset($_GET['parking'])) {
 <header>
     <form action="index.php" methods="GET">
         <select class="form-select w-25" aria-label="Default select example" name='parking'>
-
+            <option value="0 && 1">Hotels</option>
             <option value='1'>Hotel con parcheggio</option>
             <option value='0'>Hotel senza parcheggio</option>
         </select>
+        <input type="text" name="vote" placeholder=" testo da inserire">
         <button type="submit">Cerca</button>
     </form>
 
