554afa26cf6192d7ecf71eb389e19aed62bfb7f9
luca-masera <lucamasera1995@gmail.com>
2023-11-29 15:56:22 +0100

    tabella main

--- partials/main.php
+++ partials/main.php
@@ -3,12 +3,39 @@
 ?>
 
 <main>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $hotel) { ?>
+                <tr>
 
-
-    <?php foreach ($hotels as $hotel) {
-        echo '<div>' . $hotel['name'] . $hotel['description'] . $hotel['parking'] . $hotel['vote'] . $hotel['distance_to_center'] . '</div>';
-    }
-
-    ?>
+                    <td>
+                        <?php echo $hotel['name'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['description'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['parking'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['vote'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['distance_to_center'] ?>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 
 </main>
+{}
